93e73b7049d1069c6ab396d332eb5716af7581ef
Sravan NK <hiraone@proton.me>
2025-01-09 10:08:48 +0530

    FloatingTypes.kt file created and write float and double codes and compare the differences of both

---
+++ app/src/main/java/com/learn/kotlinbasics/FloatingTypes.kt
@@ -0,0 +1,14 @@
+package com.learn.kotlinbasics
+
+fun main(){
+    var circleRatio=3.14;
+    println(circleRatio);
+    circleRatio=3.1415926535897932;
+    println(circleRatio);
+
+    //declaring float by manually because kotlin default taken as double
+    var circleRATIO=3.14f;
+    println(circleRATIO)
+    circleRATIO=3.1415926535f
+    println(circleRATIO)
+}
