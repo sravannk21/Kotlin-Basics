bd13135b98b09c314823442a01acc9499efe9a62
Sravan NK <hiraone@proton.me>
2025-01-09 09:35:46 +0530

    created variableTypes.kt file to practice val and var data types and create example for that

---
+++ app/src/main/java/com/learn/kotlinbasics/VariableTypes.kt
@@ -0,0 +1,10 @@
+package com.learn.kotlinbasics
+
+fun main(){
+    val constantNumber = 26;
+    var mutableString = "Sravan NK";
+    println(constantNumber);
+    println(mutableString);
+    mutableString="raone";
+    println(mutableString);
+}
