b2b0a73b5207b2c07d3847d3b11df5bbbb0d2141
Sravan NK <hiraone@proton.me>
2025-01-09 09:16:20 +0530

    just created example for the val and var data types in kotlin and write a brief description about these to using comment

--- app/src/main/java/com/learn/kotlinbasics/Basics.kt
+++ app/src/main/java/com/learn/kotlinbasics/Basics.kt
@@ -1,5 +1,19 @@
 package com.learn.kotlinbasics
 
 fun main(){
+
+    //example for the val
+    val name ="SRAVAN NK";
+    // val keyword is using to declare a variable but when using val keyword its value can not
+    // possible to re assign the value which means val values are immutable genarally kotlin recomented to use
+    // val keyword to declare the variables
+
+
+    //example for var
+    var user="sravan NK";
+    user="raone";
+    //var keyword is just opposite of val its a mutable datatype so we can possible to re assign the value of
+    //the declared variable
+    println(user);
     println("Hello World");
 }
