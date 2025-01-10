9e40eaa48fecc265797caec1ef76ad28673f739d
Sravan NK <hiraone@proton.me>
2025-01-10 10:55:46 +0530

    unsigned integeers.kt file created and write some example for the unsigned integers

---
+++ app/src/main/java/com/learn/kotlinbasics/UnsignedIntegers.kt
@@ -0,0 +1,12 @@
+package com.learn.kotlinbasics
+
+fun main(){
+    val age=27u;
+    println(age);
+    val age2: UByte=12u;
+    println(age2);
+}
+
+//in kotlin Unsigned integers are whole numbers that does not have positive or negative signs
+//its only represented non negative numbers which starting from zero  unsigned integer types are
+//UByte,UShort, UInt, ULong
