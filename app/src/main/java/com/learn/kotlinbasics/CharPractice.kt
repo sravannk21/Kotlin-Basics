e0c0c8113543a9bfd8fc726a9368c4dde32f95c9
Sravan NK <hiraone@proton.me>
2025-01-11 11:21:01 +0530

    charpractice.kt file created and write code to understanding about the char usage

---
+++ app/src/main/java/com/learn/kotlinbasics/CharPractice.kt
@@ -0,0 +1,6 @@
+package com.learn.kotlinbasics
+
+fun main(){
+    val myChar='s'//in char you can only store an single character. generally use char when you need to add some unicode charater in the code
+    print(myChar)
+}
